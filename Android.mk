9b77a7d9dc6cbd5f721a452d43b78a076b13d03b
DarkestSpawn <58578293+DarkestSpawn@users.noreply.github.com>
2023-12-10 12:17:07 -0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 DarkestSpawn's TWRP device tree generator
+# Copyright (C) 2023 DarkestSpawn's TWRP device tree clone
 #
 # SPDX-License-Identifier: Apache-2.0
 #
